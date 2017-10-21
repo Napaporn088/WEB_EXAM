0e3e108655ce61be1ac5f1d96a69f013a5e3d233
Napaporn088 <guideza59@hotmail.com>
2017-10-21 16:37:51 +0700

    WEB_EXAM

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
+import {Home,Home2} from './component';
+
 
 class App extends Component {
   render() {
     return (
+      <BrowserRouter>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
+          <ul>
+         
+            <NavLink to="/" activeClassName="active">Home</NavLink>&nbsp;&nbsp;&nbsp;
+            <NavLink to="/Home2" activeClassName="active">Home1</NavLink>
+          </ul>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <Switch>
+          <Route path="/" component={Home} exact={true} />
+          </Switch>
         </p>
       </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/component/Home.component.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class Home extends React.Component{
+    render() {
+        return(
+            <div>
+                <p> ชื่อ : นางสาวนภาพร  คุณยศยิ่ง </p>
+                <p> รหัสนิสิต : 57024222 </p>
+                
+            </div>
+        );
+    }
+}
+
+export default Home;
